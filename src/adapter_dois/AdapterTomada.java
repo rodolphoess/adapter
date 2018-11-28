c0592eb3eb797668c35fefc1c4032b28f0b3ca3b
Rodolpho Erick <rodolphoerick90@gmail.com>
2018-11-28 10:50:47 -0300

    Modificação em comentário de classe.

--- src/adapter_dois/AdapterTomada.java
+++ src/adapter_dois/AdapterTomada.java
@@ -1,8 +1,9 @@
 package adapter_dois;
 
 /*
- * Aqui nós temos uma tomada de dois pinos mas queremos na verdade conectar a uma tomada de três pinos.
- * O adaptador fará essa tarefa aceitando como entrada dois pinos, mas apresentando como saída a tomada de três pinos.
+ * Aqui nós temos como entrada uma tomada de dois pinos mas queremos na verdade conectar a uma tomada de três pinos como saída.
+ * O adaptador fará essa tarefa aceitando como entrada dois pinos, mas apresentando como saída a tomada de três pinos, 
+ * conforme é possível observar no método ligarNaTomadaDeDoisPinos() herdada da classe TomadaDeDoisPinos. .
  */
 public class AdapterTomada extends TomadaDeDoisPinos {
 	private TomadaDeTresPinos tomadaDeTresPinos;
